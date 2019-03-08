6a63527f42f325cac7f982f659404d10474a3e82
IltonBJSilva <xlsonplay@gmail.com>
2019-03-08 17:51:44 -0300

    Adiciona partes basicas e principais da classe Peca

--- src/br/com/sankhya/gerenciapeca/dominion/Peca.java
+++ src/br/com/sankhya/gerenciapeca/dominion/Peca.java
@@ -1,5 +1,87 @@
 package br.com.sankhya.gerenciapeca.dominion;
 
-public class Peca {
+import java.util.Date;
 
+public class Peca {
+	Integer ID;
+	String titulo;
+	Integer duracao;
+	Date data;
+	
+	public Peca() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Peca(Integer iD, String titulo, Integer duracao, Date data) {
+		super();
+		ID = iD;
+		this.titulo = titulo;
+		this.duracao = duracao;
+		this.data = data;
+	}
+
+	public Integer getID() {
+		return ID;
+	}
+
+	public void setID(Integer iD) {
+		ID = iD;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public Integer getDuracao() {
+		return duracao;
+	}
+
+	public void setDuracao(Integer duracao) {
+		this.duracao = duracao;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((ID == null) ? 0 : ID.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Peca other = (Peca) obj;
+		if (ID == null) {
+			if (other.ID != null)
+				return false;
+		} else if (!ID.equals(other.ID))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Peca [ID=" + ID + ", titulo=" + titulo + ", duracao=" + duracao + ", data=" + data + "]";
+	}
+	
+	
+	
 }
