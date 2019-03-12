59ac10f32eb0c535d0a4c45ba9674621c73b6b0b
IltonBJSilva <xlsonplay@gmail.com>
2019-03-12 15:05:05 -0300

    Adiciona metodos basico na classe Papel

--- src/br/com/sankhya/gerenciapeca/dominion/Papel.java
+++ src/br/com/sankhya/gerenciapeca/dominion/Papel.java
@@ -1,9 +1,82 @@
 package br.com.sankhya.gerenciapeca.dominion;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 
-public class Papel {
+public class Papel implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
 	private Integer ID;
 	private String nomePersonagem;
 	private BigDecimal acrecismos;
+	
+	Papel(){
+		
+	}
+
+	public Papel(Integer iD, String nomePersonagem, BigDecimal acrecismos) {
+		super();
+		ID = iD;
+		this.nomePersonagem = nomePersonagem;
+		this.acrecismos = acrecismos;
+	}
+
+	public Integer getID() {
+		return ID;
+	}
+
+	public void setID(Integer iD) {
+		ID = iD;
+	}
+
+	public String getNomePersonagem() {
+		return nomePersonagem;
+	}
+
+	public void setNomePersonagem(String nomePersonagem) {
+		this.nomePersonagem = nomePersonagem;
+	}
+
+	public BigDecimal getAcrecismos() {
+		return acrecismos;
+	}
+
+	public void setAcrecismos(BigDecimal acrecismos) {
+		this.acrecismos = acrecismos;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((ID == null) ? 0 : ID.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Papel other = (Papel) obj;
+		if (ID == null) {
+			if (other.ID != null)
+				return false;
+		} else if (!ID.equals(other.ID))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Papel [ID=" + ID + ", nomePersonagem=" + nomePersonagem + ", acrecismos=" + acrecismos + "]";
+	}
+	
+	
+	
+	
+	
 }
--- src/br/com/sankhya/gerenciapeca/dominion/Peca.java
+++ src/br/com/sankhya/gerenciapeca/dominion/Peca.java
@@ -4,9 +4,7 @@ import java.io.Serializable;
 import java.util.Date;
 
 public class Peca implements Serializable{
-	/**
-	 * 
-	 */
+
 	private static final long serialVersionUID = 1L;
 	private Integer ID;
 	private String titulo;
